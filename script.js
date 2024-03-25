3773676af4e3e9c64c9b743d3e769967ff167f02
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-25 13:20:50 +0100

    array

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+let out = document.getElementById("output")
 let img = document.getElementById("image")
-let imgage = ['avatar7.png', 'avatar2.png', 'avatar3.png', 'avatar6.png', 'avatar4.png'];
+let image = ['avatar7.png', 'avatar2.png', 'avatar3.png', 'avatar6.png', 'avatar4.png'];
 let index = 0
-img.src = imgage[index] 
+img.src = image[index]
+
+let arrObj = [
+    { name: 'Dyno' },
+    { name: 'Tout', age: 5 },
+    { name: 'Full name: Celeb', age: 'age: 7 '},
+    { name: 'One more', age: 11 }
+];
+out.innerHTML += `<h3>${arrObj[2].name}</h3> <h3> ${arrObj[2].age}</h3>`;
+
+function next() {
+    if (index == image.length - 1) {
+        index = 0
+    } else {
+        index++
+    }
+    img.src = image[index]
+}
+
+function prev() {
+    if (index == 0) {
+        index = image.length - 1
+    } else {
+        index--
+    }
+    img.src = image[index]
+}
