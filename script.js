d942a27021cd2818cec6357fa13baabba333ae8e
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-25 14:34:53 +0100

    arrObj

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 let out = document.getElementById("output")
 let img = document.getElementById("image")
-let image = ['avatar7.png', 'avatar2.png', 'avatar3.png', 'avatar6.png', 'avatar4.png'];
-let index = 0
-img.src = image[index]
+let index = 0 
+
 
 let arrObj = [
-    { name: 'Dyno' },
-    { name: 'Tout', age: 5 },
-    { name: 'Full name: Celeb', age: 'age: 7 '},
-    { name: 'One more', age: 11 }
+    { name: 'Full Name: Emmanuel', age: 'Age: 25', mail: 'E-mail: fatokunemmanuel@gmail.com' , image: "avatar7.png"},
+    { name: 'Full Name: Celeb', age: 'Age: 7 ', image: "avatar2.png" },
+    { name: 'Full Name: Ayo', age: 'Age: 6 ', image: "avatar3.png" },
+    { name: 'Full Name: Tade', age: 'Age: 9 ', image: "avatar6.png" },
+    { name: 'Full Name: Tade', age: 'Age: 10 ' , image: "avatar4.png"}
 ];
-out.innerHTML += `<h3>${arrObj[2].name}</h3> <h3> ${arrObj[2].age}</h3>`;
+
+function display() {
+    img.src = arrObj[index].image
+    out.innerHTML = `<h3>${arrObj[index].name} <br> ${arrObj[index].age}</h3>`
+}
+
+display()
 
 function next() {
-    if (index == image.length - 1) {
+    if (index == arrObj.length - 1) {
         index = 0
     } else {
         index++
     }
-    img.src = image[index]
+
+    display()
 }
 
 function prev() {
     if (index == 0) {
-        index = image.length - 1
+        index = arrObj.length - 1
     } else {
         index--
     }
-    img.src = image[index]
+
+    display()
 }
+
+
